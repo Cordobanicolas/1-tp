9b9893fa1913cf14a4d24cb80680dbd641206f8b
Cordobanicolas <ncordoba508@gmail.com>
2024-06-19 10:09:30 -0300

    Create act5.py

---
+++ act5.py
@@ -0,0 +1,7 @@
+datos_personales = {'nombre': 'taylor swift', 'edad': 34, 'ciudad': 'nueva york'}
+print("Nombre:", datos_personales['nombre'])
+datos_personales['edad'] = 200
+
+datos_personales['genero'] = 'femenino'
+
+print("Lista", datos_personales)
